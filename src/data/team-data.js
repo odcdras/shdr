c98b2dcb80babdaf7647c1df2c9a7a012eb64e12
ranjeet <ranjeetranjan.raj@gmail.com>
2023-08-28 21:52:07 +0530

    fix image

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -1,21 +1,21 @@
 export const teamData = [
   {
-    img: "/src/assets/img/clinic-1.jpg",
+    img: "/img/clinic-1.jpg",
     name: "Clinic 1",
     position: "Address 1",
   },
   {
-    img: "/src/assets/img/clinic-2.jpg",
+    img: "/img/clinic-2.jpg",
     name: "Clinic 2",
     position: "Address 2",
   },
   {
-    img: "/src/assets/img/clinic-3.jpg",
+    img: "/img/clinic-3.jpg",
     name: "Clinic 3",
     position: "Address 3",
   },
   {
-    img: "/src/assets/img/clinic-4.jpg",
+    img: "/img/clinic-4.jpg",
     name: "Clinic 4",
     position: "Address 4",
   },
